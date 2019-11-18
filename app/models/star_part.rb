9231ad189440134db0220ffac43a87332af7b8f1
Sergio Delgado <sergio@dgtl.ec>
2019-11-18 22:59:47 +0100

    cloudinary installation

--- app/models/star_part.rb
+++ app/models/star_part.rb
@@ -3,4 +3,5 @@ class StarPart < ApplicationRecord
   has_many :bookings
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
   validates :category, inclusion: { in: ["head", "bust", "bottom"] }
+  has_one_attached :photo
 end
