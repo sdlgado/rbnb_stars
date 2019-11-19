f343af7f206c40794ee27754743bcea2eea4b7b5
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-19 14:10:50 +0100

    back end Pundit done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  # include Pundit
+  skip_after_action :verify_authorized, only: [:home, :account] # pour Pundit
+  skip_before_action :authenticate_user!, only: [:home, :account]
+
   def home
   end
 end
--- app/controllers/star_parts_controller.rb
+++ app/controllers/star_parts_controller.rb
@@ -1,9 +1,30 @@
 class StarPartsController < ApplicationController
+    before_action :set_star_part, only: [:show, :edit, :update, :destroy]
+
   def index
-    @star_parts = StarPart.all
+    # @star_parts = StarPart.all
+    @star_parts = policy_scope(StarPart).order(created_at: :desc) # Pundit
   end
 
   def show
     @star_part = StarPart.find(params[:id])
+  end
+
+  def new
+    @star_part = current_user.star_parts.new
+    authorize @star_part
+  end
+
+  def create
+    @star_part = current_user.star_parts.new(star_part_params)
+    authorize @star_part
+    if @star_part.save
+      redirect_to @star_part
+    end
+  end
+
+  def set_star_part
+    @star_part = StarPart.find(params[:id])
+    authorize @star_part
   end
 end
