475eddc85acaf0a06edbb00e848f7ce2ed57b830
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-19 16:18:42 +0100

    destroy working

--- app/models/star_part.rb
+++ app/models/star_part.rb
@@ -1,6 +1,6 @@
 class StarPart < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
   validates :category, inclusion: { in: ["head", "bust", "bottom"] }
   has_one_attached :photo
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/account", to: "users#account"
   resources :star_parts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # resources :star_parts, only:[:destroy], as: :star_parts
+  # end
 end
