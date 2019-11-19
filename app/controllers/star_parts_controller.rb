0bb27b12702d71e7f3f57d9c3bb3e5ab25428e91
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-19 15:09:30 +0100

    begin the routes edit and delete and update

--- app/controllers/star_parts_controller.rb
+++ app/controllers/star_parts_controller.rb
@@ -15,12 +15,28 @@ class StarPartsController < ApplicationController
     authorize @star_part
   end
 
+  def edit
+    @star_part = StarPart.find(params[:id])
+  end
+
   def create
     @star_part = current_user.star_parts.new(star_part_params)
     authorize @star_part
     if @star_part.save
       redirect_to @star_part
     end
+  end
+
+  def update
+    @star_part = StarPart.find(params[:id])
+    @star_part.update(star_part_params)
+    redirect_to star_parts_path
+  end
+
+  def destroy
+    @star_part = StarPart.find(params[:id])
+    @star_part.destroy
+    redirect_to star_parts_path
   end
 
   def set_star_part
