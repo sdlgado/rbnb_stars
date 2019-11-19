bb45e04e059ad862b61914c33e2063374f495f76
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-19 18:03:55 +0100

    skiped athorization for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,12 @@
 class BookingsController < ApplicationController
   def new
+    skip_authorization
     @booking = Booking.new
     @star_part = StarPart.find(params[:star_part_id])
   end
 
   def create
+    skip_authorization
     @star_part = StarPart.find(params[:star_part_id])
     start_date = Date.parse(params[:booking][:start_date])
     end_date = Date.parse(params[:booking][:end_date])
