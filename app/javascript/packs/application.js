7843e70c44cb5979b965a1804be1f51a18ad1f60
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-20 15:37:35 +0100

    deleted carousel from js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import {toggleDateInputs} from "../plugins/flatpickr"
 import $ from 'jquery';
-import { initCarousel } from '../components/carousel';
 import { navbarTransparent } from '../components/navbar';
 
 toggleDateInputs();
