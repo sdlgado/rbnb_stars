22ae1a8645e5c03d4627c7fc31fd9c253c26e9f6
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-20 15:38:08 +0100

    javascript code replace in pack js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,10 @@ import { navbarTransparent } from '../components/navbar';
 
 toggleDateInputs();
 navbarTransparent();
+
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
