46cb1db8ca306b6e5faebed1573f76d183ce0ada
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-21 17:38:22 +0100

    fixed bug

--- app/javascript/components/text_typed.js
+++ app/javascript/components/text_typed.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#typed-js', {
-    strings: ["Head", "Head Eyes", "Bust  Jason Statham  Abs", "Los Angeles  Cardi B", "And more !"],
-    typeSpeed: 50,
-    loop: true
-  });
+  const banner = document.getElementById('#typed-js');
+  if (banner) {
+    new Typed('#typed-js', {
+      strings: ["Head", "Head Eyes", "Bust  Jason Statham  Abs", "Los Angeles  Cardi B", "And more !"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
