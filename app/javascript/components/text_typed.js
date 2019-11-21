50d586698ecdf1aa287715df54e44aedfdab42be
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-21 14:31:18 +0100

    bug fixed

--- app/javascript/components/text_typed.js
+++ app/javascript/components/text_typed.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#typed-js', {
-    strings: ["Head - Gad Elmaleh's Eyes", "Bottom - Nicki Minaj's Butt", "Bust - Jason Statham's Abs"],
-    typeSpeed: 50,
-    loop: true
-  });
+  const banner = document.getElementById('typed-js');
+  if (banner) {
+    new Typed('#typed-js', {
+      strings: ["Head - Gad Elmaleh's Eyes", "Bottom - Nicki Minaj's Butt", "Bust - Jason Statham's Abs"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
