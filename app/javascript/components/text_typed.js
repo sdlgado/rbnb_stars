28810a1f4fe3d953b510339cb9be4993013d5d7f
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-21 14:43:36 +0100

    search bar in the middle

--- app/javascript/components/text_typed.js
+++ app/javascript/components/text_typed.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#typed-js', {
-    strings: ["Head  Gad Elmaleh Eyes", "Bust  Jason Statham  Abs", "Los Angeles  Cardi B", "and more"],
+    strings: ["Head", "Head Eyes", "Bust  Jason Statham  Abs", "Los Angeles  Cardi B", "And more !"],
     typeSpeed: 50,
     loop: true
   });
