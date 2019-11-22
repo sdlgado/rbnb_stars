0e2e82ccdddccb2493e90c6023dd12df02deacd9
Adelaide Blot <adelaide.blot@hotmail.fr>
2019-11-22 11:34:37 +0100

    validation new part

--- app/controllers/star_parts_controller.rb
+++ app/controllers/star_parts_controller.rb
@@ -45,7 +45,7 @@ class StarPartsController < ApplicationController
   def create
     @star_part = StarPart.new(star_part_params.merge(user_id: current_user.id).merge(rating: 0)) # pour le user_id
     authorize @star_part
-    if @star_part.save!
+    if @star_part.save
       redirect_to star_parts_path
     else
       render :new
@@ -76,6 +76,6 @@ class StarPartsController < ApplicationController
   end
 
   def star_part_params
-    params.require(:star_part).permit(:description, :name_of_part, :name_of_star, :price, :category, :user_id, :photo)
+    params.require(:star_part).permit(:description, :name_of_part, :name_of_star, :price, :category, :user_id, :photo, :address)
   end
 end
